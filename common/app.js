58e80aac93bb10cae3e3fe5fde48c844c3065073
MyScouts <dev.huy.57@gmail.com>
2022-02-21 00:56:36 +0700

    init seed

--- common/app.js
+++ common/app.js
@@ -2,7 +2,23 @@ require('dotenv').config()
 
 const APP_PORT = process.env.PORT || 3000
 
+const CUSTOM_LABELS = {
+    totalDocs: 'itemCount',
+    docs: 'items',
+    limit: 'perPage',
+    page: 'currentPage',
+    nextPage: 'next',
+    prevPage: 'prev',
+    totalPages: 'pageCount',
+};
+
+const pageConfig = (page, pageSize) => {
+    return { page: parseInt(page), limit: parseInt(pageSize), customLabels: CUSTOM_LABELS }
+}
+
+
 // Export module
 module.exports = {
-    APP_PORT
+    APP_PORT,
+    pageConfig
 }
---
+++ controllers/productController.js
@@ -0,0 +1,5 @@
+const getAllProducts = (req, res) => {
+    const { page, pageSize, search } = req.value.query;
+
+
+}
