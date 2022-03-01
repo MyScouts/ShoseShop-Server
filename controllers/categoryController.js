6e8b08598384ab64d8bb1b08ed120482218e25d8
MyScouts <53051372+MyScouts@users.noreply.github.com>
2022-03-01 10:29:25 +0700

    ABC

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,7 +2,7 @@ const { responseSuccess } = require("../common/app");
 const CategoryModel = require("../models/category");
 
 const getAllIds = async (req, res) => {
-    const categories = await CategoryModel.find({}, { _id: 0, CategoryId: 1, CategoryName: 1, CategoryDescription: 1 });
+    const categories = await CategoryModel.find({}, { _id: 0, CategoryId: 1, CategoryName: 1, CategoryDescription: 1 }).sort({ CategoryName: 1 });
     return responseSuccess(res, 200, "Get all categories successfully", categories);
 }
 
