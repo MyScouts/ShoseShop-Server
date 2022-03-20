744a1d2d60d55e9d9e9ca8c9e5ccb5c533285d3b
Trần Hoàng Huy <fxhuytran99@gmail.com>
2022-03-20 10:55:20 +0700

    add images

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -6,6 +6,19 @@ const getAllIds = async (req, res) => {
     return responseSuccess(res, 200, "Get all categories successfully", categories);
 }
 
+const newCategory = async (req, res) => {
+    const { categoryName, categoryDescription } = req.body;
+    const lastCategory = await CategoryModel.findOne({}, { _id: 0, CategoryId: 1 }).sort({ CategoryId: -1 });
+    const newCategory = new CategoryModel({
+        CategoryId: lastCategory ? lastCategory.CategoryId + 1 : 1,
+        CategoryName: categoryName,
+        CategoryDescription: categoryDescription
+    });
+    const category = await newCategory.save();
+    return responseSuccess(res, 200, "Create new category successfully", category);
+}
+
 module.exports = {
-    getAllIds
+    getAllIds,
+    newCategory
 }
--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,7 +1,12 @@
 const router = require('express-promise-router')()
 const categoryController = require('../controllers/categoryController')
 
+
+router.route('/')
+    .post(categoryController.newCategory)
+
 router.route('/get-all')
     .get(categoryController.getAllIds)
+
 
 module.exports = router
